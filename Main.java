cde72e8531e6061be8b605af08c6e901c4fa733e
sputnika <sputnika91@gmail.com>
2019-05-23 13:19:19 -0400

    Greetings

--- Main.java
+++ Main.java
@@ -1,9 +1,25 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+// import Scanner
+
+import java.util.Scanner;
+
+
 public class Main {
 
     public static void main(String[] args ){
+        Scanner myName = new Scanner(System.in); 
+        System.out.println("What is your name?");
+        
+        String name = myName.nextLine();
+        
+        if((name == "Alice") || (name == "Bob")){
+         
+        } 
+        System.out.println("Greetings " + name);
+        
 
     }
 }
